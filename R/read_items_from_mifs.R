78f3fc1c4a8727d2f3407edb57f27a9be6a8eeee
Johannes Koch <jokoch@pik-potsdam.de>
2020-04-23 17:48:57 +0200

    :sparkles:  Implemented parllel processing in read_items_from_mifs

--- R/read_items_from_mifs.R
+++ R/read_items_from_mifs.R
@@ -9,6 +9,7 @@
 #'   to extract from the mif files.
 #' @param returnAsMagpie Logical indicating return type. Either list of quitte
 #'   objects, or magpie.
+#' @param n_cores Integer indicating the number of cores to be used in parallel
 #'
 #' @return my_data Either a list of quitte objects (default) or a single magpie
 #'   object,
@@ -21,13 +22,21 @@
 #' @importFrom pbapply pblapply
 read_items_from_mifs <- function (mif_filepaths,
                                   regexs_of_items,
-                                  returnAsMagpie = FALSE) {
+                                  returnAsMagpie = FALSE,
+                                  n_cores = 1) {
   # Collpase the regexs into a single regex
   regex <- paste(regexs_of_items, collapse = "|")
 
   # Read and filter the mifs
-  my_data <- pblapply(mif_filepaths, read.report, as.list = FALSE) %>%
-    lapply(function(y) y[,,grep(regex, getNames(y), value=T)])
+  if (n_cores == 1) {
+    my_data <- pblapply(mif_filepaths,
+                        function(y) read.report(y, as.list = FALSE) %>% `[`(,,grep(regex, getNames(.), value=T)))
+  } else {
+    my_data <- mclapply(mif_filepaths,
+                        function(y) read.report(y, as.list = FALSE) %>% `[`(,,grep(regex, getNames(.), value=T)),
+                        mc.cores = n_cores)
+  }
+
 
   # Depending on retrun type use mbind or as.quitte
   if (returnAsMagpie) {
