947945e4c62e379d655383e34aaa526e75dd79b9
Johannes Koch <jokoch@pik-potsdam.de>
2023-10-17 15:53:25 +0200

    Adapt reading from mifs. Improve reading time by factor 10

--- R/read_items_from_mifs.R
+++ R/read_items_from_mifs.R
@@ -4,28 +4,26 @@
 #'
 #' @param mif_filepaths A vector of strings containing the file paths.
 #' @param regexs_of_items A vector of regex patterns identifying the variables to extract.
-#' @param returnAsMagpie If TRUE return magclass object instead of a list of tibbles.
+#' @param col_types A string with the col_type specification for readr::read_delim
 #'
-#' @return A list of tibbles (default) or a single magpie object.
+#' @return A tibble
 #' @export
-read_items_from_mifs <- function (mif_filepaths, regexs_of_items, returnAsMagpie = FALSE) {
+read_items_from_mifs <- function (mif_filepaths, regexs_of_items, col_types = "cccccddddddddddddddddddd-") {
+  rlang::check_installed("readr")
+
   # Collapse the regexs into a single regex
   regex <- paste0("(", paste(regexs_of_items, collapse = ")|("), ")")
 
-  # Read and filter the mifs
-  my_data <- purrr::map(mif_filepaths,
-                        function(y) {
-                          h1 <- magclass::read.report(y, as.list = FALSE)
-                          h2 <- h1[, , grep(regex, magclass::getNames(h1), value = TRUE)]
-                          return(h2)
-                        })
-
-  # Depending on retrun type use mbind or as.quitte
-  if (returnAsMagpie) {
-    my_data <- magclass::mbind(as.vector(my_data))
-  } else {
-    my_data <- purrr::map(my_data, tibble::as_tibble)
-  }
-
-  my_data
+  purrr::map(mif_filepaths, function(x) {
+    readr::read_delim(x,
+                      delim = ";",
+                      na = "N/A",
+                      col_types = col_types,
+                      name_repair = "minimal",
+                      progress = FALSE,
+                      lazy = TRUE) %>%
+      dplyr::filter(grepl(regex, .data$Variable)) %>%
+      tidyr::pivot_longer(tidyselect::starts_with("2"), names_to = "year") %>%
+      dplyr::rename_with(tolower)
+  }) %>% purrr::list_rbind()
 }
